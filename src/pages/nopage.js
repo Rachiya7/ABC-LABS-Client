5199462febd65c08fba7071eafc54e9376d92f6f
Rachitha Gimhara <102872783+Rachiya7@users.noreply.github.com>
2024-03-25 13:32:11 +0530

    Add files via upload

---
+++ src/pages/nopage.js
@@ -0,0 +1,44 @@
+import "../styles/component.css";
+import "../styles/layout.css";
+import Glyphicon from "../png/image-from-rawpixel-id-3819993-png.png";
+import { CiMedicalCross, CiMedicalClipboard } from "react-icons/ci";
+import { FaHandHoldingMedical, FaHouseMedical } from "react-icons/fa6";
+export default function NoPage() {
+  return (
+    <>
+    <div className="page-name-con">
+      <div className="page-bg-min-con-nav">
+          <div className="page-background-nav page-back-1-nav">
+            <FaHouseMedical />
+          </div>
+          <div className="page-background-nav page-back-3-nav">
+            <CiMedicalCross />
+          </div>
+          <div className="page-background-nav page-back-2-nav">
+            <FaHandHoldingMedical />
+          </div>
+          <div className="page-background-nav page-back-4-nav">
+            <CiMedicalCross />
+          </div>
+         
+          <div className="page-background-nav page-back-5-nav">
+            <CiMedicalClipboard />
+          </div>
+        </div>
+        <div className="page-name-spaceholder"></div>
+        <span className="page-log-span">
+         
+        </span>
+        <h1 className="page-name-h1"></h1>
+      </div>
+
+      <div className="welcome-user">
+      <h1>Welcome to ABC Laboratories</h1>
+        <div className="welcom-img-con">
+          <img className="welcom-img" src={Glyphicon} alt="" />
+        </div>
+       
+      </div>
+    </>
+  );
+}
